529c78fdff6345c0623fab89461e43fe8a48b16c
JulioCesarBernalMendez <juliocesarbernalmendez@hotmail.com>
2023-01-26 18:13:14 -0600

    Included CMSIS libraries, startup, linker and makefile

--- 1_ToggleLed/main.c
+++ 1_ToggleLed/main.c
@@ -1,5 +1,12 @@
 #include <stdint.h>
-#include "main.h"
+#include "stm32f0xx.h"
+
+#define GPIOA_EN_BIT        (0x01U << 17)
+
+#define GPIOx_PIN_5         (0x01U << 5)
+#define LED_PIN             (GPIOx_PIN_5)
+
+#define GPIOA_CLK_ENBL()    (RCC->AHBENR |= GPIOA_EN_BIT)
 
 void GPIOA_Init( void );
 void toggle_led( void );
